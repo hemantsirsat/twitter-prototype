2b9ccb4830573a77a57701507528bc2f44d6f055
hemantsirsat <hemantsirsat40@gmail.com>
2020-05-13 21:38:22 +0530

    initial commit after format

--- common/database.py
+++ common/database.py
@@ -4,8 +4,8 @@ import os
 
 class Database:
 
-    URI = os.environ.get('MONGOLAB_URI')
-    DATABASE = pymongo.MongoClient(URI).get_database()
+    URI = 'mongodb://127.0.0.1:27017/twitter'
+    DATABASE = pymongo.MongoClient(URI).get_default_database()
 
     def insert(collection:str,data:Dict):
         Database.DATABASE[collection].insert(data)
